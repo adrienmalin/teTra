054fac4f09f0866bf793228db2fa209d8e876e56
adrien <adrien@malingrey.fr>
2023-05-17 21:29:12 +0200

    camera up

--- app.js
+++ app.js
@@ -778,8 +778,8 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 400)
-camera.position.set(5, 6.5, 16)
-camera.lookAt(5, 8.5, 0)
+camera.position.set(5, 7, 16)
+camera.lookAt(5, 9, 0) 
 
 const commonCylinderGeometry = new THREE.CylinderGeometry(25, 25, 400, 20, 1, true)
 
