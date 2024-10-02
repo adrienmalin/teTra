5c2eaca35a685c235e1ab07b63836185384fc839
adrien <adrien@malingrey.fr>
2024-10-03 00:38:10 +0200

    remove import

--- jsm/Menu.js
+++ jsm/Menu.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
-import { Mino, environment } from './Tetrominoes.js'
+import { environment } from './Tetrominoes.js'
 
 
 export class Menu extends GUI {
